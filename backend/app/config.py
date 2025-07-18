d478507e68a61e3209780f1112be3c6feb2a1ec0
58divya <divyalamichhane85@gmail.com>
2025-07-19 02:22:34 +1200

    Docker file

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,11 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()  # loads from .env in root folder
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
 
     
     SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///whanau.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',  'sqlite:///' + os.path.join(basedir, 'database', 'whanau.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret-jwt')
