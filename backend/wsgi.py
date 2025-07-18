2202306a1f27d2bacfc429df10b863fecb6b87d4
58divya <divyalamichhane85@gmail.com>
2025-07-19 04:56:27 +1200

    added build folder

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,5 +1,6 @@
 from app import create_app
 from dotenv import load_dotenv
+from flask import Flask, send_from_directory
 import os
 
 load_dotenv()  # This loads .env automatically
@@ -7,5 +8,12 @@ load_dotenv()  # This loads .env automatically
 app = create_app()
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app = Flask(__name__, static_folder="build")
+
+@app.route("/")
+@app.route("/<path:path>")
+def serve(path="index.html"):
+    if os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
